527cf6d674954ab306b07be7b726ba0faa1d8cb7
kyeongmin Kim <65937523+akswlakswl1@users.noreply.github.com>
2024-04-01 20:28:50 +0900

    Create test.py

---
+++ baekjoon/2884/test.py
@@ -0,0 +1,15 @@
+#시간과 관련된 문제
+# M<45일때 달라지는 것을 잘 파악 했어야 했다.
+
+H,M = map(int,input().split())
+
+if M <45:
+    M = M+15
+    if H == 0:
+        H = 23
+    else:
+        H = H-1
+else:
+    M = M-45
+    
+print(H,M)
