1921f1c951c5f2a2db733582f327662e21a13956
kyeongmin Kim <65937523+akswlakswl1@users.noreply.github.com>
2024-06-10 13:47:16 +0900

    재귀

--- baekjoon/10872/app.py
+++ baekjoon/10872/app.py
@@ -2,10 +2,9 @@
 #재귀 함수는 순차적으로 호출을 하고 역순으로 종료한다.
 
 def factorial(n):
-    result = 1
-    if n>0:
-        result = n* factorial(n-1)
-    return result
+    if n <= 1:
+        return 1
+    return n * factorial(n-1)
 
 n = int(input())
 print(factorial(n))
